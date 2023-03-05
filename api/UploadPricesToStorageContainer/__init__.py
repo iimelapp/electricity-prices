7de4d7859a9f2f07f980c4b878a71f5b35f3913d
Iina Lappalainen <iimelapp@student.jyu.fi>
2023-03-05 18:09:52 +0200

    create iac and workflow files

--- api/UploadPricesToStorageContainer/__init__.py
+++ api/UploadPricesToStorageContainer/__init__.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import os
+import logging
 from azure.storage.blob import BlobServiceClient, BlobClient
 import azure.functions as func
 
@@ -14,25 +15,36 @@ def parse_date(date):
     return f"{day}-{hour}"
 
 def main(mytimer: func.TimerRequest):
+    logging.info('fetching')
     response_api = requests.get('https://api.porssisahko.net/v1/latest-prices.json')
+    logging.info('fetched')
     data = response_api.text
     data_obj = json.loads(data)
     container_name = "prices"
+
+    logging.info('create clients')
 
     blob_service_client = BlobServiceClient.from_connection_string(
             AZURE_STORAGE_CONNECTION_STRING)
 
     container_client = blob_service_client.get_container_client(
             container_name)
+    
+    logging.info('check if exists')
 
     if not container_client.exists():
-        print('creating new container %s', container_name)
+        logging.info('creating new container %s', container_name)
         blob_service_client.create_container(container_name)
 
+    logging.info('crete blobss')
     for item in data_obj["prices"]:
         blob_name = f"{parse_date(item['startDate'])}.json"
         new_blob = BlobClient.from_connection_string(
             AZURE_STORAGE_CONNECTION_STRING, container_name, blob_name)
         if not new_blob.exists():
-            print('creating new blob', blob_name)
+            logging.info(blob_name)
+            logging.info(type(blob_name))
+            logging.info('creating new blob %s', blob_name)
             new_blob.upload_blob(json.dumps(item))
+
+    logging.info('done')
---
+++ terraform/main.tf
@@ -0,0 +1,111 @@
+provider "archive" {
+  
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "518aba77-825e-43b5-8e33-5dbdeaf4d8ac"
+  tenant_id         = "30374e8b-89ee-46cb-8064-f659cea96f13"
+  client_id         = "8b44d6fa-6d97-40c1-972b-09e5e2a952c6"
+  client_secret     = "qBK8Q~PSd6fz8C5N2LJw4GkXsBoQDrjMgfKEkdyT"
+}
+
+locals {
+  local_api_zip_name = "electricity-prices-${formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())}.zip"
+}
+
+data "archive_file" "zipfile" {
+  type = "zip"
+  output_path = "../api/${local.local_api_zip_name}"
+  source_dir = "../api"
+}
+
+resource "azurerm_resource_group" "electricity-prices-rg" {
+  name     = "electricity-prices-rg"
+  location = "North Europe"
+}
+
+resource "azurerm_storage_account" "electricity-prices-sa" {
+  name                     = "epstorageaccount220614"
+  resource_group_name      = azurerm_resource_group.electricity-prices-rg.name
+  location                 = azurerm_resource_group.electricity-prices-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "ep-app-service-plan" {
+  name                     = "ep-app-service-plan"
+  resource_group_name      = azurerm_resource_group.electricity-prices-rg.name
+  location                 = azurerm_resource_group.electricity-prices-rg.location
+  os_type                  = "Linux"
+  sku_name                 = "Y1"
+}
+
+resource "azurerm_linux_function_app" "electricity-prices-app" {
+  name                = "electricity-prices-app-220614"
+  resource_group_name      = azurerm_resource_group.electricity-prices-rg.name
+  location                 = azurerm_resource_group.electricity-prices-rg.location
+
+  storage_account_name       = azurerm_storage_account.electricity-prices-sa.name
+  storage_account_access_key = azurerm_storage_account.electricity-prices-sa.primary_access_key
+  service_plan_id            = azurerm_service_plan.ep-app-service-plan.id
+
+  app_settings = {
+      "WEBSITE_RUN_FROM_PACKAGE" = "https://${azurerm_storage_account.electricity-prices-sa.name}.blob.core.windows.net/${azurerm_storage_container.api.name}/${azurerm_storage_blob.function_blob.name}${data.azurerm_storage_account_blob_container_sas.blob_container_sas.sas}"
+      APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app-insights.connection_string
+      AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.electricity-prices-sa.primary_connection_string
+    }
+
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
+}
+
+resource "azurerm_storage_container" "api" {
+  name = "api"
+  storage_account_name = azurerm_storage_account.electricity-prices-sa.name
+}
+
+resource "azurerm_storage_blob" "function_blob" {
+  name = local.local_api_zip_name
+  storage_account_name = azurerm_storage_account.electricity-prices-sa.name
+  storage_container_name = azurerm_storage_container.api.name
+  type = "Block"
+  source = "../api/${local.local_api_zip_name}"
+}
+
+data "azurerm_storage_account_blob_container_sas" "blob_container_sas" {
+  connection_string = azurerm_storage_account.electricity-prices-sa.primary_connection_string
+  container_name = azurerm_storage_container.api.name
+
+  start = "2023-01-01T00:00:00Z"
+  expiry = "2025-01-01T00:00:00Z"
+
+  permissions {
+    read = true
+    add = false
+    create = false
+    write = false
+    delete = false
+    list = false
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
+  name                = "log-analytics-workspace"
+  resource_group_name      = azurerm_resource_group.electricity-prices-rg.name
+  location                 = azurerm_resource_group.electricity-prices-rg.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "app-insights" {
+  name                = "appinsights"
+  resource_group_name      = azurerm_resource_group.electricity-prices-rg.name
+  location                 = azurerm_resource_group.electricity-prices-rg.location
+  workspace_id        = azurerm_log_analytics_workspace.log-analytics-workspace.id
+  application_type    = "web"
+}
